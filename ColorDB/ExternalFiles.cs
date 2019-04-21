27d42041e467121f5090578d736b9dd76ee7825a
KaliszS <skaliszoo@gmail.com>
2019-04-21 02:28:14 +0200

    Poprawiona obsługa wyjątków dla wczytywania z pliku

--- ColorDB/ExternalFiles.cs
+++ ColorDB/ExternalFiles.cs
@@ -24,6 +24,8 @@ namespace ColorDB
                             r = Convert.ToInt32(parts[0]);
                             g = Convert.ToInt32(parts[1]);
                             b = Convert.ToInt32(parts[2]);
+                            if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+                                throw new ArgumentOutOfRangeException();
 
                             Palette.AddColor(n, r, g, b);
 
@@ -37,7 +39,12 @@ namespace ColorDB
                 Console.WriteLine("Nie znaleziono pliku o podanej nazwie!\nWcisnij enter, aby wrocic do menu.");
                 Console.ReadLine(); Menu.ShowMenu(-1);
             }
-            
+            catch (ArgumentOutOfRangeException)
+            {    // dodac ile kolor dodalo, a ile nie (palette2.txt)
+                Console.WriteLine("Bajt miesci tylko liczby z zakresu 0-255!\nWcisnij enter, aby wrocic do menu, a nastepnie popraw blad w pliku.");
+                Console.ReadLine(); Menu.ShowMenu(-1);
+            }
+
             Console.Clear();
             Console.WriteLine("Dane z pliku {0}.txt wczytano poprawnie.\nWcisnij enter, aby zobaczyc zmiany w palecie.", fileName);
             Console.ReadLine();  Palette.ShowPalette();
